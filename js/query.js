9ad82d98bed8140616defdb57905646046e44de4
ibrahimnsu15 <ibrahimcoxs11@gmail.com>
2021-01-14 23:42:23 +0600

    Implement calculation

--- js/query.js
+++ js/query.js
@@ -62,6 +62,7 @@ function show_cut_list() {
 }
 
 function configureInputField() {
+  $("#resultId").hide();
   $.ajax({
     url: "../data.json",
     async: false,
@@ -95,4 +96,165 @@ function configureInputField() {
       //end
     },
   });
+}
+
+function convertToGram(value) {
+  if ($("#gSelectId").attr("aria-selected") == "true") {
+    return value;
+  }
+
+  return parseInt(value * 28.35);
+}
+
+function convertToMM(value) {
+  if ($("#mmSelectId").attr("aria-selected") == "true") {
+    return value;
+  }
+  return parseInt(value * 25.4);
+}
+
+function calculate() {
+  let doneness = $("#selectDoneness").find(":selected").text();
+  let weightOrSize = "weight";
+  let inputValue = 0;
+  if ($("#thicknessSelectButtonID").attr("aria-selected") == "true") {
+    weightOrSize = "size";
+    inputValue = $("#thicknessInputId").val();
+    inputValue = parseInt(inputValue);
+    inputValue = convertToGram(inputValue);
+  } else {
+    inputValue = $("#weightInputId").val();
+    inputValue = parseInt(inputValue);
+    inputValue = convertToGram(inputValue);
+  }
+
+  $.ajax({
+    url: "../data.json",
+    async: false,
+    dataType: "json",
+    success: function (response) {
+      // do stuff with response.
+      let index = sessionStorage.getItem("index");
+      let cutCode = sessionStorage.getItem("cutCode");
+      if (weightOrSize == "weight") {
+        if (response[index].cuts[cutCode].weight[doneness].values.length < 2) {
+          set_result(
+            response[index].cuts[cutCode].weight[doneness].values[0]
+              .temperature,
+            response[index].cuts[cutCode].weight[doneness].values[0].duration
+          );
+        } else {
+          let flag = true;
+          let max = 1000;
+          for (let i in response[index].cuts[cutCode].weight[doneness].values) {
+            console.log(inputValue);
+            console.log(
+              "Low " +
+                response[index].cuts[cutCode].weight[doneness].values[i].low
+            );
+            console.log(
+              "High " +
+                response[index].cuts[cutCode].weight[doneness].values[i].high
+            );
+            console.log(
+              response[index].cuts[cutCode].weight[doneness].values[i].low <=
+                inputValue
+            );
+            console.log(
+              response[index].cuts[cutCode].weight[doneness].values[i].high >=
+                inputValue
+            );
+
+            if (
+              response[index].cuts[cutCode].weight[doneness].values[i].low <=
+                inputValue &&
+              response[index].cuts[cutCode].weight[doneness].values[i].high >=
+                inputValue
+            ) {
+              set_result(
+                response[index].cuts[cutCode].weight[doneness].values[i]
+                  .temperature,
+                response[index].cuts[cutCode].weight[doneness].values[i]
+                  .duration
+              );
+              flag = false;
+              break;
+            }
+            max = response[index].cuts[cutCode].weight[doneness].values[i].high;
+          }
+
+          if (flag) {
+            alert("Weight should be less then " + max + "g");
+          }
+        }
+      } else {
+        if (response[index].cuts[cutCode].size[doneness].values.length < 2) {
+          set_result(
+            response[index].cuts[cutCode].size[doneness].values[0].temperature,
+            response[index].cuts[cutCode].size[doneness].values[0].duration
+          );
+        } else {
+          let flag = true;
+          let max = 1000;
+          for (let i in response[index].cuts[cutCode].size[doneness].values) {
+            if (
+              response[index].cuts[cutCode].size[doneness].values[i].size >=
+              inputValue
+            ) {
+              set_result(
+                response[index].cuts[cutCode].weight[doneness].values[i]
+                  .temperature,
+                response[index].cuts[cutCode].weight[doneness].values[i]
+                  .duration
+              );
+              flag = false;
+              break;
+            }
+            max = response[index].cuts[cutCode].size[doneness].values[i].size;
+          }
+          if (flag) {
+            alert("Size should be less then " + max + "mm");
+          }
+        }
+      }
+
+      //end
+    },
+  });
+}
+
+function set_result(temperature, duration) {
+  $("#resultId").show();
+
+  if ($("#cSelectId").attr("aria-selected") == "true") {
+    console.log("Test1");
+    $("#temperatureResult").text(temperature);
+    $("#temperatureSymbolId").html("&#8451;");
+  } else {
+    console.log("Test");
+    let f = temperature * (9 / 5) + 32;
+    f = f.toFixed(2);
+    $("#temperatureResult").text(f);
+    $("#temperatureSymbolId").html("&#8457;");
+  }
+
+  $("#durationResult").text(customTimeformat(duration));
+}
+
+function customTimeformat(mm) {
+  let hh = parseInt(mm / 60);
+
+  mm = mm % 60;
+  let timeStr = "";
+  if (hh > 0) {
+    if (hh == 1) {
+      timeStr = hh + " hour ";
+    } else {
+      timeStr = hh + " hours ";
+    }
+  }
+  if (mm > 0) {
+    timeStr = timeStr + mm + " mins";
+  }
+  return timeStr;
 }
